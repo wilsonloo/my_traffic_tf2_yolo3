0dd0ad1155ab9f8cc8e67d661371392eca865148
wilson <wilson@miyou.ubuntu>
2020-09-28 06:30:06 +0800

    抓取视频时，可以保存

--- video_demo.py
+++ video_demo.py
@@ -20,22 +20,31 @@ from PIL import Image
 
 
 return_elements = ["input/input_data:0", "pred_sbbox/concat_2:0", "pred_mbbox/concat_2:0", "pred_lbbox/concat_2:0"]
-pb_file         = "./yolov3_coco.pb"
-video_path      = "./docs/images/road.mp4"
+pb_file         = "./models/yolov3_800_chn_test_loss_2.8650.freed.pb"
+video_path      = "./docs/images/Rec0003.mp4"
 # video_path      = 0
-num_classes     = 80
-input_size      = 416
+num_classes     = 43
+input_size      = 800
+score_threshold = 0.25
+iou_threshold   = 0.45
 graph           = tf.Graph()
 return_tensors  = utils.read_pb_return_tensors(graph, pb_file, return_elements)
 
-with tf.Session(graph=graph) as sess:
-    vid = cv2.VideoCapture(video_path)
+vid = cv2.VideoCapture(video_path)
+size = (int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    
+#第二个参数为视频编码格式，第三个参数为帧率，第四个参数为每帧大小
+out = cv2.VideoWriter('./docs/images/save.mp4', cv2.VideoWriter_fourcc(*'mp4v'),20, size)
+
+with tf.compat.v1.compat.v1.Session(graph=graph) as sess:
     while True:
         return_value, frame = vid.read()
         if return_value:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             image = Image.fromarray(frame)
         else:
+            out.release()
+            vid.release
             raise ValueError("No image!")
         frame_size = frame.shape[:2]
         image_data = utils.image_preporcess(np.copy(frame), [input_size, input_size])
@@ -50,8 +59,8 @@ with tf.Session(graph=graph) as sess:
                                     np.reshape(pred_mbbox, (-1, 5 + num_classes)),
                                     np.reshape(pred_lbbox, (-1, 5 + num_classes))], axis=0)
 
-        bboxes = utils.postprocess_boxes(pred_bbox, frame_size, input_size, 0.3)
-        bboxes = utils.nms(bboxes, 0.45, method='nms')
+        bboxes = utils.postprocess_boxes(pred_bbox, frame_size, input_size, score_threshold)
+        bboxes = utils.nms(bboxes, iou_threshold, method='nms')
         image = utils.draw_bbox(frame, bboxes)
 
         curr_time = time.time()
@@ -61,8 +70,5 @@ with tf.Session(graph=graph) as sess:
         cv2.namedWindow("result", cv2.WINDOW_AUTOSIZE)
         result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         cv2.imshow("result", result)
+        out.write(result)
         if cv2.waitKey(1) & 0xFF == ord('q'): break
-
-
-
-
